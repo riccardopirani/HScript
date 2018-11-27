405a2faab07051823687ea8adc511d3ebdb18a47
riccardopirani <riccardopirani@users.noreply.github.com>
2018-11-27 16:58:28 +0100

    Update upgrade-debian.sh

--- upgrade-debian.sh
+++ upgrade-debian.sh
@@ -4,6 +4,8 @@
 
 #install favourite package
 sudo apt-get install  -f fish gcc
+#update docker images
+docker images |grep -v REPOSITORY|awk '{print $1}'|xargs -L1 docker pull 
 #remove lock apt
 sudo rm /var/lib/apt/lists/lock
 sudo rm /var/cache/apt/archives/lock
